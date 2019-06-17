75cc8beb2b84f88325ba53f6745f619f65356c74
sunhaolin <sunhaolin@hotoa.com>
2019-06-17 16:18:12 +0800

    ssl 使用dzgy证书

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -5,7 +5,7 @@ APP_HOST=${APP_HOST:-app}
 APP_PORT_NUMBER=${APP_PORT_NUMBER:-8000}
 
 # Check if SSL should be enabled (if certificates exists)
-if [ -f "/cert/fullchain1.pem" -a -f "/cert/privkey1.pem" ]; then
+if [ -f "/cert/dzug.cn.crt" -a -f "/cert/dzug.cn.key" ]; then
   echo "found certificate and key, linking ssl config"
   ssl="-ssl"
 else
